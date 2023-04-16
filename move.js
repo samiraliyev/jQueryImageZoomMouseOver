541ef6cb09aa302a8a28581bf6121f78a85e485c
samiraliyev <samir.aliyev@code.edu.az>
2023-04-16 21:08:03 +0400

    Add files via upload

---
+++ move.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.zoom').zoom();
+    $('.thumb').on('click', 'a', function(e) {
+      e.preventDefault();
+      var thumb = $(e.delegateTarget);
+      if (!thumb.hasClass('active')) {
+        thumb.addClass('active').siblings().removeClass('active');
+        $('.zoom')
+          .zoom({
+            url: this.href
+          })
+          .find('img').attr('src', this.href);
+      }
+    });
+  });
+  
